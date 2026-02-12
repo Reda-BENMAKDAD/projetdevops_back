5c98c3d106eeba25952b027f8a55ccf76c8271d4
reda <redabenmakdad@gmail.com>
2026-02-12 13:47:47 +0100

    feature: Suppression de la configuration d'un environnement de développement dans le conteneur backend car inutile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 set -e
 
-# Ce script est utilisé pour lancer soit des migrations soit un deploy selon l'environnement (production ou developpement)
-# définit dans la variable NODE_ENV
+# Ce script est utilisé pour deployer la migration de prisma  
 
 if [ "$NODE_ENV" = "production" ]; then
     echo "Environment: PRODUCTION"
     echo "lancement du deploy de prisma"
     npx prisma migrate deploy
-else
-    echo "Environment: DEVELOPPEMENT"
-    echo "⚠️ Lancez 'npx prisma migrate dev' manuellement dans le conteneur pour appliquer les migrations."
 fi
 
 
-echo "Starting application..."
+echo "Démarrage de l'application..."
 exec "$@"
