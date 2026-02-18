93209b2f7b691592e8e31a6159e451663871ce5e
reda <redabenmakdad@gmail.com>
2026-02-18 18:39:51 +0100

    fix: correction du healthcheck du service de base de données, pour que l'utilisateur checké sois app et non root. ajout de la variable d'environnement MYSQL_HOST pour pointer sur le conteneur docker

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import 'dotenv/config';
 
 const adapter = new PrismaMariaDb({
+  host: process.env.MYSQL_HOST,
   database: process.env.MYSQL_DATABASE,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD 
