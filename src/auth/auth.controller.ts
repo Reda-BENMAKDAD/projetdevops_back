6546a3ea5e100bba1e0027bbe968cfb0c527d9fe
reda <redabenmakdad@gmail.com>
2026-02-18 14:44:50 +0100

    feature: migration vers prisma version 7.4.0

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,12 +8,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() body: RegisterDto) {
+  register(@Body() body) {
+    console.log('Registration attempt:', body);
     return this.authService.register(body);
   }
 
   @Post('login')
   login(@Body() body: LoginDto) {
+    console.log('Login attempt:', body);
     return this.authService.login(body);
   }
 }
